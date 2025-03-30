ef523b3529781d7a08ffebb953403cab8d4e78bb
techking11 <nma1132000@gmail.com>
2025-03-30 13:13:54 +0700

    start: environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div>
-      <h1>New Components</h1>
+    <div className="bg-red-900">
+      <h1 className="text-white text-3xl">New Components</h1>
     </div>
   );
 }
