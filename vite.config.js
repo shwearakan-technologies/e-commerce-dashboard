491103620953f7370bb84c9047ac6b1043531e23
techking11 <nma1132000@gmail.com>
2025-03-31 13:06:24 +0700

    layout: create project layout

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     host: "localhost",
     port: "8000",
