01e989714b3dd71de6ba15cff8cb8d75bd6c992c
techking11 <nma1132000@gmail.com>
2025-03-31 13:04:28 +0700

    component: add Navbar

---
+++ src/ui/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Bell, Menu, Sun } from 'lucide-react';
+
+export default function Navbar() {
+  return (
+    <div className="z-30 border-b border-gray-800 bg-base-100 px-3">
+      <div className="navbar mx-auto lg:container">
+        <div className="navbar-start">
+          <div className="dropdown block lg:hidden">
+            <label
+              htmlFor="my-drawer-2"
+              role="button"
+              className="btn btn-circle btn-ghost"
+            >
+              <Menu />
+            </label>
+          </div>
+        </div>
+        <div className="navbar-end gap-5">
+          <Bell />
+          <Sun />
+        </div>
+      </div>
+    </div>
+  );
+}
